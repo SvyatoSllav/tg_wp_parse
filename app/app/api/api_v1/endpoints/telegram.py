472ae6558e34deda1931e0b2216426ae846fc248
Александр <tarasov.slavas2002@gmail.com>
2023-02-01 20:32:39 +0000

    Логика системы телеграм + телетон

---
+++ app/app/api/api_v1/endpoints/telegram.py
@@ -0,0 +1,41 @@
+from fastapi import APIRouter, Depends, Query
+from dependency_injector.wiring import inject, Provide
+
+from app.core.containers import Container
+from app.api.deps import commit_and_close_session
+
+from app.schemas.messengers import MessengerIn
+
+
+router = APIRouter()
+
+
+@router.get("/receive_confirm_code")
+@inject
+@commit_and_close_session
+async def get_confirmation_code(
+        tg_messenger_id: str,
+        telegram_service = Depends(Provide[Container.telegram_service])):
+    """Отправляет в личку мессенджера код подтверждения."""
+    return await telegram_service.receive_code(tg_messenger_id=tg_messenger_id)
+
+
+@router.post("/authorize_messenger")
+@inject
+@commit_and_close_session
+async def authorize_messenger(
+        tg_messenger_id: str,
+        code: str,
+        telegram_service = Depends(Provide[Container.telegram_service])):
+    """Отправляет в личку мессенджера код подтверждения."""
+    return await telegram_service.authorize_messenger(tg_messenger_id=tg_messenger_id, code=code)
+
+
+@router.post("/me")
+@inject
+@commit_and_close_session
+async def me(
+        tg_messenger_id: str,
+        telegram_service = Depends(Provide[Container.telegram_service])):
+    """Отправляет в личку мессенджера код подтверждения."""
+    return await telegram_service.me(tg_messenger_id=tg_messenger_id)
---
+++ app/app/services/telegram.py
@@ -0,0 +1,58 @@
+from fastapi.responses import JSONResponse
+
+from telethon import TelegramClient
+
+from app.models.messenger import Messenger
+
+from app.repository.messenger import RepositoryMessenger
+
+
+class TelegramService:
+
+    def __init__(
+            self,
+            repository_messenger: RepositoryMessenger) -> None:
+        self._repository_messenger = repository_messenger
+
+    @staticmethod
+    def session_name(phone: str, api_id: str):
+        return f"{phone}_{api_id}"
+
+    async def receive_code(self, tg_messenger_id: str):
+        messenger = self._repository_messenger.get(id=tg_messenger_id)
+        phone, api_id, api_hash = messenger.phone, messenger.api_id, messenger.api_token 
+        session_name = self.session_name(phone=phone, api_id=api_id)
+
+        client = TelegramClient(session_name, api_id, api_hash)
+        await client.connect()
+        phone_hash = await client.send_code_request(phone=phone)
+        self._repository_messenger.update(db_obj=messenger, obj_in={"phone_hash": phone_hash.phone_code_hash})
+        await client.disconnect()
+        return str(phone_hash)
+
+    async def authorize_messenger(self, tg_messenger_id: str, code: str):
+        messenger = self._repository_messenger.get(id=tg_messenger_id)
+        phone, phone_hash, api_id, api_hash = messenger.phone, messenger.phone_hash, messenger.api_id, messenger.api_token 
+        session_name = self.session_name(phone=phone, api_id=api_id)
+
+        client = TelegramClient(session_name, api_id, api_hash)
+        await client.connect()
+        await client.sign_in(
+            phone=phone,
+            code=code,
+            phone_code_hash=phone_hash,
+        )
+        self._repository_messenger.update(db_obj=messenger, obj_in={"is_active": True})
+        client.disconnect()
+        return JSONResponse(content="Успешно подключено", status_code=200)
+
+    async def me(self, tg_messenger_id):
+        messenger = self._repository_messenger.get(id=tg_messenger_id)
+        phone, phone_hash, api_id, api_hash = messenger.phone, messenger.phone_hash, messenger.api_id, messenger.api_token 
+        session_name = self.session_name(phone=phone, api_id=api_id)
+
+        client = TelegramClient(session_name, api_id, api_hash)
+        await client.connect()
+        me = await client.get_me()
+        await client.disconnect()
+        return me.first_name
